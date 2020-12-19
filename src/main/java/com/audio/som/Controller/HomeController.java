617b59b7eac77eeca6bd61bf90221eb8afcc21fd
Éverton <foreverplayer95@gmail.com>
2020-12-19 20:50:02 -0300

    mudanças no CSS e alguns comentários

--- src/main/java/com/audio/som/Controller/HomeController.java
+++ src/main/java/com/audio/som/Controller/HomeController.java
@@ -14,26 +14,26 @@ public class HomeController {
   
    @Autowired
    Database database;
-
+    /* Visualizar */
    @RequestMapping(value = "/", method = RequestMethod.GET)
    public ModelAndView listar(){
        ModelAndView mav = new ModelAndView("index");
        mav.addObject("ed", database.findAll());
        return mav;
    }
-
+   /* Salvar */
    @RequestMapping(value = "/", method = RequestMethod.POST)
    public String index(ExibirModal exibirModal) {
        database.save(exibirModal);
        return "redirect:/";
    }
-
+   /* Deletar */
    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
    public String delete(@PathVariable("id") int id){
    database.deleteById(id);
    return "redirect:/";
    }
-
+   /* Atualizar */
    @RequestMapping(value = "/update/{id}", method = RequestMethod.GET)
    public String update(@PathVariable("id") Integer id, Model model){
    model.addAttribute("dados", database.findById(id));
